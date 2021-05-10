c6d4b9ba3bf8249e6fe876693324f114b9de3c1c
郭文博 <18236958896@139.com>
2021-05-10 11:07:23 +0800

    clear

--- src/connectForm.js
+++
@@ -1,9 +0,0 @@
-import Taro from '@tarojs/taro';
-import { useForm } from './useForm';
-
-export const connectForm = Component => {
-  return props => {
-    const form = useForm();
-    return <Component {...props} form={form} />;
-  };
-};
